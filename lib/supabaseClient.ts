294e797e484274618d6aee872f82e47afd747227
Daniel-Abhinav <daniel.abhinav.r@gmail.com>
2026-04-18 00:34:23 +0530

    alternate database

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck
-const supabaseUrl = 'https://zahijhnprduytnyhilhj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphaGlqaG5wcmR1eXRueWhpbGhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1NzcyMTAsImV4cCI6MjA3MjE1MzIxMH0.xkm4RYWvE5zBaNTwjK4y5ipW2GMZOK7KKgmvEjhsujs';
+const supabaseUrl = 'https://gxfwswslamykjhjlmzqx.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4Zndzd3NsYW15a2poamxtenF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0MzkyMjgsImV4cCI6MjA5MjAxNTIyOH0.aieKFbl95A8RYYxYsyPh41wfc_7xCBoUDwmKSPC1-qs';
 
 // Fix: Reference the global `supabase` object from the CDN, which is attached to the window object.
 export const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
